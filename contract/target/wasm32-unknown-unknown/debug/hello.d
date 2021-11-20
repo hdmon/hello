8f869bb54bc010a8c3296203d1fc8503b0efe412
ntx50 <tranquy6338@gmail.com>
2021-11-20 19:57:10 +0700

    first commit

---
+++ contract/target/wasm32-unknown-unknown/debug/hello.d
@@ -0,0 +1,1 @@
+/home/binh/projects/nearvember/hello-world-near-challenge/contract/target/wasm32-unknown-unknown/debug/hello.wasm: /home/binh/projects/nearvember/hello-world-near-challenge/contract/src/lib.rs
---
+++ contract/target/wasm32-unknown-unknown/debug/libhello.d
@@ -0,0 +1,1 @@
+/home/binh/projects/nearvember/hello-world-near-challenge/contract/target/wasm32-unknown-unknown/debug/libhello.rlib: /home/binh/projects/nearvember/hello-world-near-challenge/contract/src/lib.rs
